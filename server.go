094fc1da367ab7e64d638dbb818956056d028ebf
Manuel Werder <manuelwerder@hotmail.com>
2026-05-03 22:21:45 +0200

    Adding Profiling

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 
 	"github.com/RagnaCron/linko-starter/internal/store"
@@ -36,6 +37,8 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 
 	mux.HandleFunc("GET /", s.handlerIndex)
 	mux.Handle("GET /metrics", promhttp.Handler())
+	mux.Handle("GET /debug/pprof/", s.authMiddleware(http.HandlerFunc(pprof.Index)))
+	mux.Handle("GET /debug/pprof/profile/", s.authMiddleware(http.HandlerFunc(pprof.Profile)))
 	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
 	mux.Handle("GET /api/stats", s.authMiddleware(http.HandlerFunc(s.handlerStats)))
